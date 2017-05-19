bfb3eac5ea1e3921198cc1e6673a206a9936031a
Dan <danielmillst@gmail.com>
2017-05-19 14:16:26 -0400

    re

--- src/main/java/org/cyberpwn/vortex/aperture/RemoteInstance.java
+++ src/main/java/org/cyberpwn/vortex/aperture/RemoteInstance.java
@@ -1,5 +1,6 @@
 package org.cyberpwn.vortex.aperture;
 
+import java.util.UUID;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
@@ -18,10 +19,10 @@ public interface RemoteInstance
 		{
 			if(i.getType().equals(EntityType.PLAYER))
 			{
-				return new RemotePlayer(i.getEntityId(), ((Player) i).getName(), i.getUniqueId());
+				return new RemotePlayer(40978 - i.getEntityId(), ((Player) i).getName(), UUID.nameUUIDFromBytes(i.getUniqueId().toString().getBytes()));
 			}
 			
-			return new RemoteEntity((int) (-i.getEntityId()), i.getType());
+			return new RemoteEntity((int) (4097800 - i.getEntityId()), i.getType());
 		}
 		
 		return null;
