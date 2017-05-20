b564e0e294d69d26d9bfc98f8d531a64e7f7be40
Dan <danielmillst@gmail.com>
2017-05-19 21:17:50 -0400

    Catch bukkit version

---
+++ src/main/java/wraith/VersionBukkit.java
@@ -0,0 +1,62 @@
+package wraith;
+
+import org.bukkit.Bukkit;
+
+public enum VersionBukkit
+{
+	VU,
+	V7,
+	V8,
+	V9,
+	V11,
+	V111;	
+	public static boolean tc()
+	{
+		if(get().equals(V7))
+		{
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public static boolean uc()
+	{
+		if(get().equals(V7) || get().equals(V8))
+		{
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public static VersionBukkit get()
+	{
+		if(Bukkit.getBukkitVersion().startsWith("1.7"))
+		{
+			return V7;
+		}
+		
+		if(Bukkit.getBukkitVersion().startsWith("1.8"))
+		{
+			return V8;
+		}
+		
+		if(Bukkit.getBukkitVersion().startsWith("1.9"))
+		{
+			return V9;
+		}
+		
+		if(Bukkit.getBukkitVersion().startsWith("1.10"))
+		{
+			return V11;
+		}
+		
+		if(Bukkit.getBukkitVersion().startsWith("1.11"))
+		{
+			return V111;
+		}
+		
+		return VU;
+	}
+}
