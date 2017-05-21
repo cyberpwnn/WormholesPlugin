f6a6eab27ec744843c136d8c16a31c92dfaf6242
Dan <danielmillst@gmail.com>
2017-05-21 04:36:03 -0400

    Wormhole unlink event

---
+++ src/main/java/org/cyberpwn/vortex/event/WormholeUnlinkEvent.java
@@ -0,0 +1,11 @@
+package org.cyberpwn.vortex.event;
+
+import org.cyberpwn.vortex.portal.Portal;
+
+public class WormholeUnlinkEvent extends PortalEvent
+{
+	public WormholeUnlinkEvent(Portal portal)
+	{
+		super(portal);
+	}
+}
