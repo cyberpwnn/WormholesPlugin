e79aa8035563d370b5a2673f50059d7a00391945
Dan <danielmillst@gmail.com>
2017-05-22 09:31:30 -0400

    Settings reflect

--- src/main/java/org/cyberpwn/vortex/Settings.java
+++ src/main/java/org/cyberpwn/vortex/Settings.java
@@ -1,7 +1,9 @@
 package org.cyberpwn.vortex;
 
+import java.lang.reflect.Field;
 import org.cyberpwn.vortex.config.Experimental;
 import wraith.Comment;
+import wraith.DataCluster;
 
 public class Settings
 {
@@ -27,10 +29,6 @@ public class Settings
 	public static int PROJECTION_MAX_SPEED = 5;
 	
 	@Experimental
-	@Comment("Increasing this value will make projections less accurate, but slightly improve performance\nMust be 1 or higher.")
-	public static int PROJECTION_ACCURACY = 1;
-	
-	@Experimental
 	@Comment("The interval in which portals are checked to ensure they are populated with projection maps\nThis is only called one time when the portal is created.")
 	public static int NETWORK_POPULATE_MAPPING_INTERVAL = 20;
 	
@@ -41,4 +39,104 @@ public class Settings
 	@Experimental
 	@Comment("This adds compression to projection packets through bungeecord.\nIncreasing this past 4 increases processing time and slightly reduces the size\nTesting shows comp 4 shows the best improvement in size for compression time\nMust be from 1-9")
 	public static int NETWORK_COMPRESSION_LEVEL = 4;
+	
+	public static DataCluster getConfig()
+	{
+		DataCluster cc = new DataCluster();
+		
+		for(Field i : Settings.class.getDeclaredFields())
+		{
+			try
+			{
+				String name = i.getName().toLowerCase().replaceAll("-", "_");
+				Object value = i.get(null);
+				
+				if(!i.isAnnotationPresent(Experimental.class))
+				{
+					cc.trySet("wormholes." + name, value, ((Comment) i.getDeclaredAnnotation(Comment.class)).value());
+				}
+			}
+			
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+		
+		return cc;
+	}
+	
+	public static void setConfig(DataCluster cc)
+	{
+		for(Field i : Settings.class.getDeclaredFields())
+		{
+			try
+			{
+				String name = i.getName().toLowerCase().replaceAll("-", "_");
+				
+				if(!i.isAnnotationPresent(Experimental.class))
+				{
+					if(cc.contains("wormholes." + name))
+					{
+						i.set(null, cc.getAbstract("wormholes." + name));
+					}
+				}
+			}
+			
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static DataCluster getExperimentalConfig()
+	{
+		DataCluster cc = new DataCluster();
+		
+		for(Field i : Settings.class.getDeclaredFields())
+		{
+			try
+			{
+				String name = i.getName().toLowerCase().replaceAll("-", "_");
+				Object value = i.get(null);
+				
+				if(i.isAnnotationPresent(Experimental.class))
+				{
+					cc.trySet("experimental." + name, value, ((Comment) i.getDeclaredAnnotation(Comment.class)).value());
+				}
+			}
+			
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+		
+		return cc;
+	}
+	
+	public static void setExperimentalConfig(DataCluster cc)
+	{
+		for(Field i : Settings.class.getDeclaredFields())
+		{
+			try
+			{
+				String name = i.getName().toLowerCase().replaceAll("-", "_");
+				
+				if(i.isAnnotationPresent(Experimental.class))
+				{
+					if(cc.contains("experimental." + name))
+					{
+						i.set(null, cc.getAbstract("experimental." + name));
+					}
+				}
+			}
+			
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
 }
