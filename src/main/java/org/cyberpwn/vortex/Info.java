d6fd03528975501fcb2fefe62d0b0d865137dc20
Dan <danielmillst@gmail.com>
2017-05-23 08:57:32 -0400

    Permissions & Tags

---
+++ src/main/java/org/cyberpwn/vortex/Info.java
@@ -0,0 +1,16 @@
+package org.cyberpwn.vortex;
+
+import wraith.C;
+import wraith.TXT;
+
+public class Info
+{
+	public static String TAG = TXT.makeTag(C.DARK_GRAY, C.LIGHT_PURPLE, C.GRAY, "W");
+	
+	public static String PERM_RELOAD = "wormholes.reload";
+	public static String PERM_LIST = "wormholes.list";
+	public static String PERM_CREATE = "wormholes.create";
+	public static String PERM_DESTROY = "wormholes.destroy";
+	public static String PERM_BUILD = "wormholes.build";
+	public static String PERM_USE = "wormholes.use";
+}
---
+++ src/main/java/org/cyberpwn/vortex/config/Permissable.java
@@ -0,0 +1,74 @@
+package org.cyberpwn.vortex.config;
+
+import org.bukkit.command.CommandSender;
+import org.cyberpwn.vortex.Info;
+
+public class Permissable
+{
+	private boolean canReload;
+	private boolean canCreate;
+	private boolean canDestroy;
+	private boolean canList;
+	private boolean canUse;
+	private boolean canBuild;
+	private CommandSender p;
+	
+	public Permissable(CommandSender p)
+	{
+		this.p = p;
+		canReload = has(Info.PERM_RELOAD);
+		canCreate = has(Info.PERM_CREATE);
+		canDestroy = has(Info.PERM_DESTROY);
+		canList = has(Info.PERM_LIST);
+		canUse = has(Info.PERM_USE);
+		canBuild = has(Info.PERM_BUILD);
+	}
+	
+	public boolean has(String... any)
+	{
+		for(String i : any)
+		{
+			if(p.hasPermission(i))
+			{
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	public boolean canReload()
+	{
+		return canReload;
+	}
+	
+	public boolean canCreate()
+	{
+		return canCreate;
+	}
+	
+	public boolean canDestroy()
+	{
+		return canDestroy;
+	}
+	
+	public boolean canList()
+	{
+		return canList;
+	}
+	
+	public boolean canUse()
+	{
+		return canUse;
+	}
+	
+	public boolean canBuild()
+	{
+		return canBuild;
+	}
+	
+	public CommandSender getP()
+	{
+		return p;
+	}
+}
