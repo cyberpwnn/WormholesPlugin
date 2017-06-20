b06eb7045c66e80d918fab059ef31b40f5b8f7bf
Dan <danielmillst@gmail.com>
2017-06-20 07:33:40 -0400

    Wrmhole intercept

--- src/main/java/com/volmit/wormholes/wormhole/Wormhole.java
+++ src/main/java/com/volmit/wormholes/wormhole/Wormhole.java
@@ -1,5 +1,6 @@
 package com.volmit.wormholes.wormhole;
 
+import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import com.volmit.wormholes.portal.LocalPortal;
 import com.volmit.wormholes.portal.Portal;
@@ -11,7 +12,7 @@ public interface Wormhole
 	
 	public Portal getDestination();
 	
-	public void push(Entity e);
+	public void push(Entity e, Location intercept);
 	
 	public GList<WormholeFilter> getFilters();
 }
