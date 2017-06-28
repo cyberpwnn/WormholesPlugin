491ba294607e30880c2ee87eab62d4775ac6c50b
Dan <danielmillst@gmail.com>
2017-06-28 12:18:13 -0400

    IOService handle skin proper

--- src/main/java/com/volmit/wormholes/service/IOService.java
+++ src/main/java/com/volmit/wormholes/service/IOService.java
@@ -1,13 +1,21 @@
 package com.volmit.wormholes.service;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 import org.apache.commons.io.FileUtils;
 import com.volmit.wormholes.Settings;
 import com.volmit.wormholes.Wormholes;
 import com.volmit.wormholes.util.DataCluster;
 import com.volmit.wormholes.util.JSONObject;
+import com.volmit.wormholes.util.SkinProperties;
 import com.volmit.wormholes.util.YAMLDataInput;
 import com.volmit.wormholes.util.YAMLDataOutput;
 
@@ -16,6 +24,71 @@ public class IOService
 	public IOService()
 	{
 		doConfig();
+	}
+	
+	public boolean hasSkin(UUID id)
+	{
+		File f = new File(Wormholes.instance.getDataFolder(), "cache");
+		File fx = new File(f, "skins");
+		File fz = new File(fx, id.toString() + ".usw");
+		fx.mkdirs();
+		
+		return fz.exists();
+	}
+	
+	public SkinProperties loadSkin(UUID id)
+	{
+		File f = new File(Wormholes.instance.getDataFolder(), "cache");
+		File fx = new File(f, "skins");
+		File fz = new File(fx, id.toString() + ".usw");
+		fx.mkdirs();
+		
+		if(fz.exists())
+		{
+			try
+			{
+				FileInputStream fin = new FileInputStream(fz);
+				GZIPInputStream gzi = new GZIPInputStream(fin);
+				DataInputStream dis = new DataInputStream(gzi);
+				String uvd = dis.readUTF();
+				String val = dis.readUTF();
+				String sig = dis.readUTF();
+				dis.close();
+				
+				return new SkinProperties(uvd, val, sig);
+			}
+			
+			catch(IOException e)
+			{
+				
+			}
+		}
+		
+		return null;
+	}
+	
+	public void saveSkin(UUID id, SkinProperties s)
+	{
+		File f = new File(Wormholes.instance.getDataFolder(), "cache");
+		File fx = new File(f, "skins");
+		File fz = new File(fx, id.toString() + ".usw");
+		fx.mkdirs();
+		
+		try
+		{
+			FileOutputStream fos = new FileOutputStream(fz, false);
+			GZIPOutputStream gzi = new GZIPOutputStream(fos);
+			DataOutputStream dos = new DataOutputStream(gzi);
+			dos.writeUTF(s.getUvd());
+			dos.writeUTF(s.getValue());
+			dos.writeUTF(s.getSignature());
+			dos.close();
+		}
+		
+		catch(IOException e)
+		{
+			
+		}
 	}
 	
 	public void doConfig()
