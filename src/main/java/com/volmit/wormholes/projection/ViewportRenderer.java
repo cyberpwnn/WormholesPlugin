53669b428c0eb2586881c3b038447723f74d5e3b
Dan <cyberpwnn@users.noreply.github.com>
2017-07-13 17:48:06 -0400

    Viewport renderer

--- src/main/java/com/volmit/wormholes/projection/ViewportRenderer.java
+++ src/main/java/com/volmit/wormholes/projection/ViewportRenderer.java
@@ -1,25 +1,6 @@
 package com.volmit.wormholes.projection;
 
-import org.bukkit.entity.Player;
-import com.volmit.wormholes.util.Cuboid;
-
-public interface ViewportRenderer
+public interface ViewportRenderer 
 {
-	public Player getPlayer();
-	
-	public Viewport getViewport();
-	
-	public int getStage();
-	
-	public int getMaxStage();
-	
-	public ProjectionSet getProjectionSet();
-	
-	public ProjectionSet getRenderedStages();
-	
-	public ProjectionSet getNonRenderedStages();
-	
-	public Cuboid getProjectionStage(int stage);
-	
-	public boolean isComplete();
+	public void render();
 }
