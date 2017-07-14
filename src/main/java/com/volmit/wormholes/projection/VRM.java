ba796321ed59cad9ff461599fa2c7a44e732394a
Dan <danielmillst@gmail.com>
2017-07-14 00:07:17 -0400

    VRM queue

--- src/main/java/com/volmit/wormholes/projection/VRM.java
+++ src/main/java/com/volmit/wormholes/projection/VRM.java
@@ -1,5 +1,8 @@
 package com.volmit.wormholes.projection;
 
+import com.volmit.wormholes.Wormholes;
+import com.volmit.wormholes.util.Execution;
+
 public class VRM implements ViewportRenderer
 {
 	private ViewportRenderer dialater;
@@ -13,17 +16,18 @@ public class VRM implements ViewportRenderer
 	
 	public VRM(VRBuilder builder, Viewport dialate, Viewport erode)
 	{
-		this.dialater = builder.build();
-		((ViewportRendererPortal)dialater).mode = RenderMode.DIALATE;
-		((ViewportRendererPortal)dialater).view = dialate;
-		((ViewportRendererPortal)dialater).stage = new RenderStage(dialate.getProjectionSet().get().size());
+		dialater = builder.build();
+		((ViewportRendererPortal) dialater).mode = RenderMode.DIALATE;
+		((ViewportRendererPortal) dialater).view = dialate;
+		((ViewportRendererPortal) dialater).stage = new RenderStage(dialate.getProjectionSet().get().size());
 		
-		this.eroder = builder.build();
-		((ViewportRendererPortal)eroder).mode = RenderMode.ERODE;
-		((ViewportRendererPortal)eroder).view = erode;
-		((ViewportRendererPortal)eroder).stage = new RenderStage(erode.getProjectionSet().get().size());
+		eroder = builder.build();
+		((ViewportRendererPortal) eroder).mode = RenderMode.ERODE;
+		((ViewportRendererPortal) eroder).view = erode;
+		((ViewportRendererPortal) eroder).stage = new RenderStage(erode.getProjectionSet().get().size());
 	}
 	
+	@Override
 	public void render()
 	{
 		eroder.render();
@@ -32,16 +36,50 @@ public class VRM implements ViewportRenderer
 	
 	public boolean isComplete()
 	{
-		return !((ViewportRendererPortal)eroder).stage.hasNextStage() || !((ViewportRendererPortal)dialater).stage.hasNextStage();
+		return !((ViewportRendererPortal) eroder).stage.hasNextStage() || !((ViewportRendererPortal) dialater).stage.hasNextStage();
 	}
 	
 	public Viewport getDialater()
 	{
-		return ((ViewportRendererPortal)dialater).view;
+		return ((ViewportRendererPortal) dialater).view;
 	}
 	
 	public Viewport getEroder()
 	{
-		return ((ViewportRendererPortal)eroder).view;
+		return ((ViewportRendererPortal) eroder).view;
+	}
+	
+	public void renderAll()
+	{
+		if(!isComplete())
+		{
+			int m = Math.max(((ViewportRendererPortal) dialater).stage.getMaxStage(), ((ViewportRendererPortal) eroder).stage.getMaxStage());
+			int c = ((ViewportRendererPortal) dialater).stage.getCurrentStage();
+			int s = m - c;
+			
+			for(int i = 0; i < s; i++)
+			{
+				Wormholes.pool.queue(new Execution()
+				{
+					@Override
+					public void run()
+					{
+						eroder.render();
+					}
+				});
+			}
+			
+			for(int i = 0; i < s; i++)
+			{
+				Wormholes.pool.queue(new Execution()
+				{
+					@Override
+					public void run()
+					{
+						dialater.render();
+					}
+				});
+			}
+		}
 	}
 }
