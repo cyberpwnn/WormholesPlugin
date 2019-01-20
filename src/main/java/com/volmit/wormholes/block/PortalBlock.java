f529c08273db02c5ce93550222f4956d3f4d463d
cyberpwn <danielmillst@gmail.com>
2019-01-20 12:12:21 -0500

    Portal Block

---
+++ src/main/java/com/volmit/wormholes/block/PortalBlock.java
@@ -0,0 +1,69 @@
+package com.volmit.wormholes.block;
+
+import org.bukkit.Location;
+
+public class PortalBlock
+{
+	private final PortalBlockType type;
+	private final Location location;
+
+	public PortalBlock(PortalBlockType type, Location location)
+	{
+		this.type = type;
+		this.location = location.clone();
+	}
+
+	public PortalBlockType getType()
+	{
+		return type;
+	}
+
+	public Location getLocation()
+	{
+		return location.clone();
+	}
+
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((location == null) ? 0 : location.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if(this == obj)
+		{
+			return true;
+		}
+		if(obj == null)
+		{
+			return false;
+		}
+		if(!(obj instanceof PortalBlock))
+		{
+			return false;
+		}
+		PortalBlock other = (PortalBlock) obj;
+		if(location == null)
+		{
+			if(other.location != null)
+			{
+				return false;
+			}
+		}
+		else if(!location.equals(other.location))
+		{
+			return false;
+		}
+		if(type != other.type)
+		{
+			return false;
+		}
+		return true;
+	}
+}
