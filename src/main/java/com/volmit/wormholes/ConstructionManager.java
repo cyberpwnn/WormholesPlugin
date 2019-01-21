5fec39664721466d5b452f5ac518911381a34fd0
cyberpwn <danielmillst@gmail.com>
2019-01-21 01:23:22 -0500

    Set blocks on end

--- src/main/java/com/volmit/wormholes/ConstructionManager.java
+++ src/main/java/com/volmit/wormholes/ConstructionManager.java
@@ -7,7 +7,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 
 import com.volmit.wormholes.util.lang.ParticleEffect;
-import com.volmit.wormholes.util.lang.S;
 
 public class ConstructionManager implements Listener
 {
@@ -18,17 +17,10 @@ public class ConstructionManager implements Listener
 
 	public void constructPortal(Player player, Set<Block> blocks)
 	{
-		new S(35)
+		Wormholes.effectManager.playPortalOpen(blocks);
+		for(Block i : blocks)
 		{
-			@Override
-			public void run()
-			{
-				Wormholes.effectManager.playPortalOpen(blocks);
-				for(Block i : blocks)
-				{
-					ParticleEffect.EXPLOSION_HUGE.display(0.6f, 1, i.getLocation(), 32);
-				}
-			}
-		};
+			ParticleEffect.PORTAL.display(0.6f, 8, i.getLocation(), 32);
+		}
 	}
 }
