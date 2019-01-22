87f133eed7979f4e34ba9ccb0ec5f5ba81531dd1
cyberpwn <danielmillst@gmail.com>
2019-01-21 19:25:46 -0500

    Structure to support corners faces and not direction, also caching

--- src/main/java/com/volmit/wormholes/portal/shape/PortalStructure.java
+++ src/main/java/com/volmit/wormholes/portal/shape/PortalStructure.java
@@ -1,49 +1,22 @@
 package com.volmit.wormholes.portal.shape;
 
-import org.bukkit.World;
+import java.util.Set;
 
-import com.volmit.wormholes.util.lang.BVector;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.util.Vector;
+
 import com.volmit.wormholes.util.lang.Cuboid;
 import com.volmit.wormholes.util.lang.Direction;
+import com.volmit.wormholes.util.lang.GMap;
+import com.volmit.wormholes.util.lang.GSet;
 
 public class PortalStructure
 {
-	private BVector center;
-	private BVector[] positions;
+	private Cuboid area;
 	private World world;
-	private Direction direction;
-
-	public Cuboid getPlane()
-	{
-		Cuboid c = new Cuboid(center.toLocation(getWorld()));
-
-		for(BVector i : positions)
-		{
-			c = c.getBoundingCuboid(new Cuboid(center.toLocation(world, i)));
-		}
-
-		return c;
-	}
-
-	public BVector getCenter()
-	{
-		return center;
-	}
-
-	public void setCenter(BVector center)
-	{
-		this.center = center;
-	}
-
-	public BVector[] getPositions()
-	{
-		return positions;
-	}
-
-	public void setPositions(BVector[] positions)
-	{
-		this.positions = positions;
-	}
+	private GMap<Direction, Cuboid> faceCache = new GMap<>();
+	private GSet<Location> cornerCache;
 
 	public World getWorld()
 	{
@@ -55,13 +28,54 @@ public class PortalStructure
 		this.world = world;
 	}
 
-	public Direction getDirection()
+	public Set<Location> getCorners()
 	{
-		return direction;
+		if(cornerCache == null)
+		{
+			cornerCache = new GSet<Location>();
+			cornerCache.add(corner(Direction.W, Direction.U, Direction.N));
+			cornerCache.add(corner(Direction.W, Direction.U, Direction.S));
+			cornerCache.add(corner(Direction.W, Direction.D, Direction.N));
+			cornerCache.add(corner(Direction.W, Direction.D, Direction.S));
+			cornerCache.add(corner(Direction.E, Direction.U, Direction.N));
+			cornerCache.add(corner(Direction.E, Direction.U, Direction.S));
+			cornerCache.add(corner(Direction.E, Direction.D, Direction.N));
+			cornerCache.add(corner(Direction.E, Direction.D, Direction.S));
+		}
+
+		return cornerCache;
 	}
 
-	public void setDirection(Direction direction)
+	private Location corner(Direction x, Direction y, Direction z)
 	{
-		this.direction = direction;
+		Vector v = getArea().getCornerVector(x, y, z);
+		return new Location(getArea().getWorld(), v.getX(), v.getY(), v.getZ());
+	}
+
+	public Cuboid getFace(Direction face)
+	{
+		if(!faceCache.containsKey(face))
+		{
+			faceCache.put(face, getArea().getFace(face.f()));
+		}
+
+		return faceCache.get(face);
+	}
+
+	public Cuboid getArea()
+	{
+		return area;
+	}
+
+	public void setArea(Cuboid area)
+	{
+		this.area = area;
+		invalidateCache();
+	}
+
+	private void invalidateCache()
+	{
+		faceCache.clear();
+		cornerCache = null;
 	}
 }
