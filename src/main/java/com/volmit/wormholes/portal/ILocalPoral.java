edf58921dfb152f0598ebaa08e142580bc2b4cce
cyberpwn <danielmillst@gmail.com>
2019-01-21 20:30:10 -0500

    Portal Types

--- src/main/java/com/volmit/wormholes/portal/ILocalPoral.java
+++ src/main/java/com/volmit/wormholes/portal/ILocalPoral.java
@@ -1,6 +1,10 @@
 package com.volmit.wormholes.portal;
 
+import com.volmit.wormholes.portal.shape.PortalStructure;
+
 public interface ILocalPoral extends IPortal
 {
+	public PortalStructure getStructure();
 
+	public PortalType getType();
 }
--- src/main/java/com/volmit/wormholes/portal/LocalPortal.java
+++ src/main/java/com/volmit/wormholes/portal/LocalPortal.java
@@ -2,10 +2,29 @@ package com.volmit.wormholes.portal;
 
 import java.util.UUID;
 
+import com.volmit.wormholes.portal.shape.PortalStructure;
+
 public class LocalPortal extends Portal implements ILocalPoral
 {
-	public LocalPortal(UUID id)
+	private final PortalStructure structure;
+	private final PortalType type;
+
+	public LocalPortal(UUID id, PortalType type, PortalStructure structure)
 	{
 		super(id);
+		this.type = type;
+		this.structure = structure;
+	}
+
+	@Override
+	public PortalStructure getStructure()
+	{
+		return structure;
+	}
+
+	@Override
+	public PortalType getType()
+	{
+		return type;
 	}
 }
