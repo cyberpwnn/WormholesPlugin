59f2663d5ff319c35cd1164b5e6371d38e6aaaaf
cyberpwn <danielmillst@gmail.com>
2019-01-21 19:35:56 -0500

    Portal Logic

--- src/main/java/com/volmit/wormholes/portal/IPortal.java
+++ src/main/java/com/volmit/wormholes/portal/IPortal.java
@@ -4,13 +4,19 @@ import java.util.UUID;
 
 public interface IPortal
 {
+	public static final UUID NO_DESTINATION = UUID.nameUUIDFromBytes("NoDestination".getBytes());
+
 	public UUID getId();
 
 	public UUID getDestination();
 
-	public void setDestination();
+	public void setDestination(UUID destination);
+
+	public void clearDestination();
+
+	public boolean hasDestination();
 
 	public String getName();
 
-	public void setName();
+	public void setName(String name);
 }
---
+++ src/main/java/com/volmit/wormholes/portal/Portal.java
@@ -0,0 +1,64 @@
+package com.volmit.wormholes.portal;
+
+import java.util.UUID;
+
+public class Portal implements IPortal
+{
+	private final UUID id;
+	private UUID destination;
+	private String name;
+
+	public Portal(UUID id)
+	{
+		this.id = id;
+		destination = NO_DESTINATION;
+	}
+
+	@Override
+	public UUID getId()
+	{
+		return id;
+	}
+
+	@Override
+	public UUID getDestination()
+	{
+		return destination;
+	}
+
+	@Override
+	public void setDestination(UUID destination)
+	{
+		if(destination == null)
+		{
+			clearDestination();
+			return;
+		}
+
+		this.destination = destination;
+	}
+
+	@Override
+	public String getName()
+	{
+		return name;
+	}
+
+	@Override
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	@Override
+	public void clearDestination()
+	{
+		setDestination(NO_DESTINATION);
+	}
+
+	@Override
+	public boolean hasDestination()
+	{
+		return !getDestination().equals(NO_DESTINATION);
+	}
+}
