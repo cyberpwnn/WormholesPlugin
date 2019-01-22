5d5ff33578aee59ff43f9b671fc9f106ad2ed58e
cyberpwn <danielmillst@gmail.com>
2019-01-22 11:14:38 -0500

    Settings

--- src/main/java/com/volmit/wormholes/Settings.java
+++ src/main/java/com/volmit/wormholes/Settings.java
@@ -4,4 +4,8 @@ public class Settings
 {
 	public static boolean ENABLE_PARTICLES = true;
 	public static int MAX_PORTAL_SIZE_CUBED = 24;
+	public static double PORTAL_CONSTRUCT_SPEED = 0.56; // 0.175
+	public static double PORTAL_COLAPSE_SPEED = 0.41;
+	public static double PORTAL_OPEN_SPEED = 1;
+	public static double PORTAL_CLOSE_SPEED = 1;
 }
