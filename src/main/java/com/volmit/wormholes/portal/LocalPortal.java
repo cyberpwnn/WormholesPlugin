904aad2abced605f4562c6c87702fbc625dbd6de
Dan <danielmillst@gmail.com>
2019-01-22 18:27:12 -0500

    Local portal project to an item

--- src/main/java/com/volmit/wormholes/portal/LocalPortal.java
+++ src/main/java/com/volmit/wormholes/portal/LocalPortal.java
@@ -3,9 +3,14 @@ package com.volmit.wormholes.portal;
 import java.util.UUID;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.Sound;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Item;
 
 import com.volmit.wormholes.Settings;
+import com.volmit.wormholes.geometry.Frustum4D;
 import com.volmit.wormholes.util.lang.M;
 import com.volmit.wormholes.util.lang.MSound;
 import com.volmit.wormholes.util.lang.ParticleEffect;
@@ -127,6 +132,37 @@ public class LocalPortal extends Portal implements ILocalPortal, IProgressivePor
 				for(int i = 0; i < getStructure().getArea().volume() / 7; i++)
 				{
 					ParticleEffect.BARRIER.display(0f, 1, getStructure().randomLocation().getBlock().getLocation().clone().add(0.5, 0.5, 0.5), 32);
+				}
+
+				for(Entity i : getStructure().getWorld().getEntities())
+				{
+					try
+					{
+						if(i.getType().equals(EntityType.DROPPED_ITEM) && ((Item) i).getItemStack().getType().equals(Material.NETHER_STAR) && i.getLocation().distanceSquared(getStructure().getCenter()) < 32 * 32)
+						{
+							Frustum4D frustum = new Frustum4D(i.getLocation(), getStructure(), 32);
+
+							for(int j = 0; j < 8; j++)
+							{
+								Location l = frustum.getRegion().random().toLocation(getStructure().getWorld());
+
+								if(frustum.contains(l))
+								{
+									ParticleEffect.HEART.display(0f, 1, l, 32);
+								}
+
+								else
+								{
+									ParticleEffect.VILLAGER_ANGRY.display(0f, 1, l, 32);
+								}
+							}
+						}
+					}
+
+					catch(Throwable e)
+					{
+						e.printStackTrace();
+					}
 				}
 
 				break;
