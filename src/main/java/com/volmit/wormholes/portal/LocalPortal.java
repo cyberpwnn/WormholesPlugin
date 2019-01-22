24bbab2604ce5110117f11a2007597376ac6feea
cyberpwn <danielmillst@gmail.com>
2019-01-21 19:37:51 -0500

    Portal sides

---
+++ src/main/java/com/volmit/wormholes/portal/LocalPortal.java
@@ -0,0 +1,11 @@
+package com.volmit.wormholes.portal;
+
+import java.util.UUID;
+
+public class LocalPortal extends Portal implements ILocalPoral
+{
+	public LocalPortal(UUID id)
+	{
+		super(id);
+	}
+}
---
+++ src/main/java/com/volmit/wormholes/portal/RemotePortal.java
@@ -0,0 +1,11 @@
+package com.volmit.wormholes.portal;
+
+import java.util.UUID;
+
+public class RemotePortal extends Portal implements IRemotePortal
+{
+	public RemotePortal(UUID id)
+	{
+		super(id);
+	}
+}
